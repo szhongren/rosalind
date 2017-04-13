ee4d74e1c78f902fe27eefcd177217c65913e2dc
Zhongren Shao <shao.zhongren@gmail.com>
2017-04-13 13:28:05 -0400

    revc done.

---
+++ revc/src/main.rs
@@ -0,0 +1,38 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+
+fn main() {
+    let mut dataset_path = PathBuf::from(env::current_dir()
+        .unwrap_or(PathBuf::from(env!("CARGO_MANIFEST_DIR"))));
+    dataset_path.push("datasets");
+    dataset_path.push("rosalind_revc.txt");
+
+    let data = read_file(dataset_path);
+    print!("{}", revc(data));
+}
+
+fn read_file(path: PathBuf) -> String {
+    let mut data = String::new();
+    let _ = File::open(path)
+        .unwrap()
+        .read_to_string(&mut data);
+    data
+}
+
+fn revc(genome: String) -> String {
+    genome
+    .trim()
+    .chars()
+    .map(|n|
+        match n {
+            'A' => 'T',
+            'C' => 'G',
+            'G' => 'C',
+            'T' => 'A',
+            _ => '?'
+        })
+    .rev()
+    .collect::<String>()
+}
---
+++ revc/target/debug/revc.d
@@ -0,0 +1,1 @@
+c:\Users\shaoz\projs\rosalind\revc\target\debug\revc.exe: c:\Users\shaoz\projs\rosalind\revc\src\main.rs
